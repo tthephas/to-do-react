d09cd4f242b871404eeb21af78ba66426e17454b
thepjos <josh.thephasdin@gmail.com>
2023-02-02 15:19:06 -0600

    added another section

--- src/MyList.js
+++ src/MyList.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react'
 import './App.css'
 import ListItem from './ListItem'
+import DoneItems from './DoneItems'
 
 class MyList extends Component {
   render() {
     let todoItems = this.props.theList.map((item, index) => (
       <ListItem task={item} key={index} />
       ))
+      let doneItems = this.props.doneList.map((item, index) => (
+        <DoneItems task={item} key={index} />
+        ))
     return (
       <div>
         <h1> Things i need to do and stop procrastinating! </h1>
           <ul>
             {todoItems}
+
+          </ul>
+          <h1> Things i did yesterday. GREAT JOB, YOU ARE SO PRODUCTIVE!  </h1>
+          <ul>
+            {doneItems}
 
           </ul>
       </div>
--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import reportWebVitals from './reportWebVitals';
 
 const toDos = ["Take out the Garbage", "go to get groceries", "shovel the driveway"]
 
+const dones = ["picked up jimmy from school", "unloaded the dishwasher", "folded my laundry"]
+
 
 ReactDOM.render(
-  <MyList theList={toDos} />,
+  <MyList theList={toDos} 
+  doneList={dones}
+  />,
   document.getElementById('root')
 );
 
